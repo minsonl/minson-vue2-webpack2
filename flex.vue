e188a1ddf5432667c314a19068f9280ab2e0cbe9
minsonl <512141260@qq.com>
2017-05-27 10:34:16 +0800

    Add files via upload

---
+++ flex.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<a href="http//:www.baidu.com">百度一下</a>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Comp from './Comp.vue'
+new Vue({
+  el: '#app',
+  components: {Comp}
+})
